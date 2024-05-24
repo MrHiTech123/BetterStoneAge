f99849e029273c7287991d7b94b185ab8ead3420
MrHiTech <mlebowitz97@gmail.com>
2024-05-24 03:23:10 -0400

    Added CutsomPotBlockEntity stuff

---
+++ src/main/java/net/mrhitech/BetterStoneAge/common/blockentities/CustomPotBlockEntity.java
@@ -0,0 +1,241 @@
+package net.mrhitech.BetterStoneAge.common.blockentities;
+
+
+import net.dries007.tfc.common.TFCTags.Fluids;
+import net.dries007.tfc.common.blockentities.AbstractFirepitBlockEntity;
+import net.dries007.tfc.common.blockentities.InventoryBlockEntity;
+import net.dries007.tfc.common.blockentities.TFCBlockEntities;
+import net.dries007.tfc.common.blocks.devices.FirepitBlock;
+import net.dries007.tfc.common.capabilities.Capabilities;
+import net.dries007.tfc.common.capabilities.DelegateFluidHandler;
+import net.dries007.tfc.common.capabilities.DelegateItemHandler;
+import net.dries007.tfc.common.capabilities.InventoryItemHandler;
+import net.dries007.tfc.common.capabilities.PartialItemHandler;
+import net.dries007.tfc.common.capabilities.SidedHandler;
+import net.dries007.tfc.common.capabilities.heat.HeatCapability;
+import net.dries007.tfc.common.container.PotContainer;
+import net.dries007.tfc.common.recipes.PotRecipe;
+import net.dries007.tfc.common.recipes.TFCRecipeTypes;
+import net.dries007.tfc.common.recipes.PotRecipe.Output;
+import net.dries007.tfc.common.recipes.inventory.EmptyInventory;
+import net.dries007.tfc.config.TFCConfig;
+import net.dries007.tfc.util.Helpers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Plane;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import net.dries007.tfc.common.blockentities.PotBlockEntity.*;
+
+public class CustomPotBlockEntity extends AbstractFirepitBlockEntity<PotInventory> {
+    public static final int SLOT_EXTRA_INPUT_START = 4;
+    public static final int SLOT_EXTRA_INPUT_END = 8;
+    private static final Component NAME = Component.translatable("tfc.block_entity.pot");
+    private final SidedHandler.Builder<IFluidHandler> sidedFluidInventory;
+    @Nullable
+    private PotRecipe.@Nullable Output output = null;
+    private @Nullable PotRecipe cachedRecipe = null;
+    private int boilingTicks = 0;
+
+    public CustomPotBlockEntity(BlockPos pos, BlockState state) {
+        super((BlockEntityType) TFCBlockEntities.POT.get(), pos, state, net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory::new, NAME);
+        this.sidedFluidInventory = new SidedHandler.Builder((IFluidHandler)this.inventory);
+        if ((Boolean)TFCConfig.SERVER.firePitEnableAutomation.get()) {
+            this.sidedInventory.on((new PartialItemHandler(this.inventory)).insert(new int[]{3}).extract(new int[]{4, 5, 6, 7, 8}), Plane.HORIZONTAL).on((new PartialItemHandler(this.inventory)).insert(new int[]{4, 5, 6, 7, 8}), new Direction[]{Direction.UP});
+            this.sidedFluidInventory.on((IFluidHandler)this.inventory, Plane.HORIZONTAL);
+        }
+
+    }
+
+    public void loadAdditional(CompoundTag nbt) {
+        if (nbt.contains("output")) {
+            this.output = Output.read(nbt.getCompound("output"));
+        }
+
+        this.boilingTicks = nbt.getInt("boilingTicks");
+        super.loadAdditional(nbt);
+    }
+
+    public void saveAdditional(CompoundTag nbt) {
+        if (this.output != null) {
+            nbt.put("output", Output.write(this.output));
+        }
+
+        nbt.putInt("boilingTicks", this.boilingTicks);
+        super.saveAdditional(nbt);
+    }
+
+    public int getSlotStackLimit(int slot) {
+        return 1;
+    }
+
+    public boolean isItemValid(int slot, ItemStack stack) {
+        return slot >= 4 && slot <= 8 || super.isItemValid(slot, stack);
+    }
+
+    protected void handleCooking() {
+        if (this.isBoiling()) {
+            assert this.cachedRecipe != null;
+
+            if (this.boilingTicks < this.cachedRecipe.getDuration()) {
+                ++this.boilingTicks;
+                if (this.boilingTicks == 1) {
+                    this.markForSync();
+                }
+            } else {
+                PotRecipe recipe = this.cachedRecipe;
+                PotRecipe.Output output = recipe.getOutput((net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory);
+                ((net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory).tank.setFluid(FluidStack.EMPTY);
+
+                for(int slot = 4; slot <= 8; ++slot) {
+                    ((net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory).setStackInSlot(slot, ((net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory).getStackInSlot(slot).getCraftingRemainingItem());
+                }
+
+                output.onFinish((net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory);
+                if (!output.isEmpty()) {
+                    this.output = output;
+                }
+
+                this.cachedRecipe = null;
+                this.boilingTicks = 0;
+                this.updateCachedRecipe();
+                this.markForSync();
+            }
+        } else if (this.boilingTicks > 0) {
+            this.boilingTicks = 0;
+            this.markForSync();
+        }
+
+    }
+
+    public void onCalendarUpdate(long ticks) {
+        assert this.level != null;
+
+        if ((Boolean)this.level.getBlockState(this.worldPosition).getValue(FirepitBlock.LIT)) {
+            HeatCapability.Remainder remainder = HeatCapability.consumeFuelForTicks(ticks, this.inventory, this.burnTicks, this.burnTemperature, 0, 3);
+            this.burnTicks = remainder.burnTicks();
+            this.burnTemperature = remainder.burnTemperature();
+            this.needsSlotUpdate = true;
+            if (remainder.ticks() > 0L) {
+                if (this.isBoiling()) {
+                    assert this.cachedRecipe != null;
+
+                    long ticksUsedWhileBurning = ticks - remainder.ticks();
+                    if (ticksUsedWhileBurning > (long)(this.cachedRecipe.getDuration() - this.boilingTicks)) {
+                        this.boilingTicks = this.cachedRecipe.getDuration();
+                        this.handleCooking();
+                    }
+                }
+
+                this.extinguish(this.level.getBlockState(this.worldPosition));
+                this.coolInstantly();
+            } else if (this.isBoiling()) {
+                this.boilingTicks = (int)((long)this.boilingTicks + ticks);
+            }
+        }
+
+    }
+
+    protected void coolInstantly() {
+        this.boilingTicks = 0;
+        this.markForSync();
+    }
+
+    protected void updateCachedRecipe() {
+        assert this.level != null;
+
+        this.cachedRecipe = (PotRecipe)this.level.getRecipeManager().getRecipeFor((RecipeType)TFCRecipeTypes.POT.get(), (net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory)this.inventory, this.level).orElse((Object)null);
+    }
+
+    public boolean isBoiling() {
+        return this.cachedRecipe != null && this.output == null && this.cachedRecipe.isHotEnough(this.temperature);
+    }
+
+    public boolean shouldRenderAsBoiling() {
+        return this.boilingTicks > 0;
+    }
+
+    public InteractionResult interactWithOutput(Player player, ItemStack stack) {
+        if (this.output != null) {
+            InteractionResult result = this.output.onInteract(this, player, stack);
+            if (this.output.isEmpty()) {
+                this.output = null;
+            }
+
+            this.markForSync();
+            return result;
+        } else {
+            return InteractionResult.PASS;
+        }
+    }
+
+    @Nullable
+    public PotRecipe.@Nullable Output getOutput() {
+        return this.output;
+    }
+
+    public <T> @NotNull LazyOptional<T> getCapability(Capability<T> cap, @Nullable Direction side) {
+        return cap == Capabilities.FLUID ? this.sidedFluidInventory.getSidedHandler(side).cast() : super.getCapability(cap, side);
+    }
+
+    public @Nullable AbstractContainerMenu createMenu(int windowID, Inventory playerInv, Player player) {
+        return PotContainer.create(this, playerInv, windowID);
+    }
+
+    public static class PotInventory implements EmptyInventory, DelegateItemHandler, DelegateFluidHandler, INBTSerializable<CompoundTag> {
+        private final net.mrhitech.BetterStoneAge.common.blockentities.CustomPotBlockEntity pot;
+        private final ItemStackHandler inventory;
+        private final FluidTank tank;
+
+        public PotInventory(InventoryBlockEntity<net.dries007.tfc.common.blockentities.PotBlockEntity.PotInventory> entity) {
+            this.pot = (net.mrhitech.BetterStoneAge.common.blockentities.CustomPotBlockEntity)entity;
+            this.inventory = new InventoryItemHandler(entity, 9);
+            this.tank = new FluidTank(1000, (fluid) -> {
+                return Helpers.isFluid(fluid.getFluid(), Fluids.USABLE_IN_POT);
+            });
+        }
+
+        public @NotNull ItemStack extractItem(int slot, int amount, boolean simulate) {
+            return this.pot.isBoiling() && slot >= 4 ? ItemStack.EMPTY : this.inventory.extractItem(slot, amount, simulate);
+        }
+
+        public IFluidHandler getFluidHandler() {
+            return this.tank;
+        }
+
+        public IItemHandlerModifiable getItemHandler() {
+            return this.inventory;
+        }
+
+        public CompoundTag serializeNBT() {
+            CompoundTag nbt = new CompoundTag();
+            nbt.put("inventory", this.inventory.serializeNBT());
+            nbt.put("tank", this.tank.writeToNBT(new CompoundTag()));
+            return nbt;
+        }
+
+        public void deserializeNBT(CompoundTag nbt) {
+            this.inventory.deserializeNBT(nbt.getCompound("inventory"));
+            this.tank.readFromNBT(nbt.getCompound("tank"));
+        }
+    }
+}
--- src/main/java/net/mrhitech/BetterStoneAge/common/blocks/devices/CustomPotBlock.java
+++ src/main/java/net/mrhitech/BetterStoneAge/common/blocks/devices/CustomPotBlock.java
@@ -4,10 +4,12 @@ import net.dries007.tfc.client.TFCSounds;
 import net.dries007.tfc.common.TFCDamageSources;
 import net.dries007.tfc.common.blockentities.AbstractFirepitBlockEntity;
 import net.dries007.tfc.common.blockentities.PotBlockEntity;
+import net.dries007.tfc.common.blockentities.TFCBlockEntities;
 import net.dries007.tfc.common.blocks.ExtendedProperties;
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.blocks.devices.PotBlock;
 import net.dries007.tfc.common.fluids.FluidHelpers;
+import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.util.Helpers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
@@ -18,6 +20,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -25,6 +28,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.mrhitech.BetterStoneAge.common.blockentities.BetterStoneAgeBlockEntities;
+import net.mrhitech.BetterStoneAge.common.item.BetterStoneAgeItems;
 
 
 import java.util.function.Supplier;
@@ -44,28 +48,21 @@ public class CustomPotBlock extends PotBlock {
         pot_block_entity_type_supplier = f_pot_block_entity_type_supplier;
 
     }
-
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-        System.out.println("Right-clicking a " + color + "-colored pot");
-        return (InteractionResult) level.getBlockEntity(pos, (pot_block_entity_type_supplier).get()).map((f_pot) -> {
-            System.out.println("Currently getting interaction result");
-            PotBlockEntity pot = (PotBlockEntity)f_pot;
+        return (InteractionResult)level.getBlockEntity(pos, (BlockEntityType) BetterStoneAgeBlockEntities.GLAZED_POTS.get(color).get()).map((f_pot) -> {
+            PotBlockEntity pot = (PotBlockEntity) f_pot;
             ItemStack stack = player.getItemInHand(hand);
             if (!pot.isBoiling() && stack.isEmpty() && player.isShiftKeyDown()) {
-                System.out.println("Pot is not boiling, and stack is empty, and the shift key is down!");
                 if ((Boolean)state.getValue(LIT)) {
-                    System.out.println("This should burn the player");
                     TFCDamageSources.pot(player, 1.0F);
-                    Helpers.playSound(level, pos, (SoundEvent) TFCSounds.ITEM_COOL.get());
+                    Helpers.playSound(level, pos, (SoundEvent)TFCSounds.ITEM_COOL.get());
                 } else {
-                    System.out.println("This should give the pot to the player");
-                    ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(stored_item.get()));
-                    AbstractFirepitBlockEntity.convertTo(level, pos, state, pot, (Block) TFCBlocks.FIREPIT.get());
+                    ItemHandlerHelper.giveItemToPlayer(player, new ItemStack((ItemLike) BetterStoneAgeItems.GLAZED_POTS.get(color).get()));
+                    AbstractFirepitBlockEntity.convertTo(level, pos, state, pot, (Block)TFCBlocks.FIREPIT.get());
                 }
 
                 return InteractionResult.sidedSuccess(level.isClientSide);
             } else if (!pot.isBoiling() && FluidHelpers.transferBetweenBlockEntityAndItem(stack, pot, player, hand)) {
-                System.out.println("Giving fluids to the pot");
                 pot.markForSync();
                 return InteractionResult.sidedSuccess(level.isClientSide);
             } else {
@@ -77,20 +74,66 @@ public class CustomPotBlock extends PotBlock {
                 }
 
                 if (tryInsertLog(player, stack, pot, result.getLocation().y - (double)pos.getY() < 0.6)) {
-                    System.out.println("I think this is inserting fluid into the pot?");
                     return InteractionResult.sidedSuccess(level.isClientSide);
                 } else {
-                    System.out.println("I'm me so I should open the pot menu");
                     if (player instanceof ServerPlayer) {
-                        System.out.println("Opening the pot menu");
                         ServerPlayer serverPlayer = (ServerPlayer)player;
                         Helpers.openScreen(serverPlayer, pot, pos);
                     }
-                    System.out.println("Returning!");
+
                     return InteractionResult.sidedSuccess(level.isClientSide);
                 }
             }
         }).orElse(InteractionResult.PASS);
     }
+
+
+//    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+//        System.out.println("Right-clicking a " + color + "-colored pot");
+//        return (InteractionResult) level.getBlockEntity(pos, (pot_block_entity_type_supplier).get()).map((f_pot) -> {
+//            System.out.println("Currently getting interaction result");
+//            PotBlockEntity pot = (PotBlockEntity)f_pot;
+//            ItemStack stack = player.getItemInHand(hand);
+//            if (!pot.isBoiling() && stack.isEmpty() && player.isShiftKeyDown()) {
+//                System.out.println("Pot is not boiling, and stack is empty, and the shift key is down!");
+//                if ((Boolean)state.getValue(LIT)) {
+//                    System.out.println("This should burn the player");
+//                    TFCDamageSources.pot(player, 1.0F);
+//                    Helpers.playSound(level, pos, (SoundEvent) TFCSounds.ITEM_COOL.get());
+//                } else {
+//                    System.out.println("This should give the pot to the player");
+//                    ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(stored_item.get()));
+//                    AbstractFirepitBlockEntity.convertTo(level, pos, state, pot, (Block) TFCBlocks.FIREPIT.get());
+//                }
+//
+//                return InteractionResult.sidedSuccess(level.isClientSide);
+//            } else if (!pot.isBoiling() && FluidHelpers.transferBetweenBlockEntityAndItem(stack, pot, player, hand)) {
+//                System.out.println("Giving fluids to the pot");
+//                pot.markForSync();
+//                return InteractionResult.sidedSuccess(level.isClientSide);
+//            } else {
+//                if (!pot.isBoiling()) {
+//                    InteractionResult interactResult = pot.interactWithOutput(player, stack);
+//                    if (interactResult != InteractionResult.PASS) {
+//                        return interactResult;
+//                    }
+//                }
+//
+//                if (tryInsertLog(player, stack, pot, result.getLocation().y - (double)pos.getY() < 0.6)) {
+//                    System.out.println("I think this is inserting fluid into the pot?");
+//                    return InteractionResult.sidedSuccess(level.isClientSide);
+//                } else {
+//                    System.out.println("I'm me so I should open the pot menu");
+//                    if (player instanceof ServerPlayer) {
+//                        System.out.println("Opening the pot menu");
+//                        ServerPlayer serverPlayer = (ServerPlayer)player;
+//                        Helpers.openScreen(serverPlayer, pot, pos);
+//                    }
+//                    System.out.println("Returning!");
+//                    return InteractionResult.sidedSuccess(level.isClientSide);
+//                }
+//            }
+//        }).orElse(InteractionResult.PASS);
+//    }
 
 }
