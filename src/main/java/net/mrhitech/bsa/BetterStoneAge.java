2af9e73a2e45fa01d08101bf7851677e1aaae314
MrHiTech <mlebowitz97@gmail.com>
2024-08-28 01:25:18 -0400

    Moved creative tabs into their own file

--- src/main/java/net/mrhitech/bsa/BetterStoneAge.java
+++ src/main/java/net/mrhitech/bsa/BetterStoneAge.java
@@ -21,6 +21,7 @@ import net.mrhitech.bsa.client.BetterStoneAgeSounds;
 import net.mrhitech.bsa.client.ClientEventHandler;
 import net.mrhitech.bsa.common.blockentities.BetterStoneAgeBlockEntities;
 import net.mrhitech.bsa.common.blocks.BetterStoneAgeBlocks;
+import net.mrhitech.bsa.common.creative.BetterStoneAgeCreativeTabs;
 import net.mrhitech.bsa.common.item.BetterStoneAgeItems;
 import net.mrhitech.bsa.common.recipes.BetterStoneAgeRecipeSerializers;
 import net.mrhitech.bsa.common.recipes.BetterStoneAgeRecipeTypes;
@@ -85,65 +86,7 @@ public class BetterStoneAge
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == TFCCreativeTabs.EARTH.tab().getKey()) {
-            for (DyeColor color : DyeColor.values()) {
-                event.accept(BetterStoneAgeItems.UNFIRED_JUGS.get(color));
-                event.accept(BetterStoneAgeItems.GLAZED_JUGS.get(color));
-                event.accept(BetterStoneAgeItems.UNFIRED_POTS.get(color));
-                event.accept(BetterStoneAgeItems.GLAZED_POTS.get(color));
-            }
-            event.accept(BetterStoneAgeItems.BONE_FISH_HOOK);
-            event.accept(BetterStoneAgeItems.BONE_FISHING_ROD);
-            event.accept(BetterStoneAgeItems.BONE_ARROWHEAD);
-            event.accept(BetterStoneAgeItems.SABERTOOTH_FANG);
-            
-        }
-        if (event.getTabKey() == TFCCreativeTabs.MISC.tab().getKey()) {
-            event.accept(BetterStoneAgeItems.SINEW);
-            event.accept(BetterStoneAgeItems.DRIED_SINEW);
-            event.accept(BetterStoneAgeItems.POUNDED_SINEW);
-            event.accept(BetterStoneAgeItems.SINEW_STRING);
-        }
-        if (event.getTabKey() == TFCCreativeTabs.FOOD.tab().getKey()) {
-            event.accept(BetterStoneAgeItems.CRUSHED_BARLEY_GRAIN);
-            event.accept(BetterStoneAgeItems.CRUSHED_OAT_GRAIN);
-            event.accept(BetterStoneAgeItems.CRUSHED_RYE_GRAIN);
-            event.accept(BetterStoneAgeItems.CRUSHED_WHEAT_GRAIN);
-
-            event.accept(BetterStoneAgeItems.COARSE_BARLEY_FLOUR);
-            event.accept(BetterStoneAgeItems.COARSE_MAIZE_FLOUR);
-            event.accept(BetterStoneAgeItems.COARSE_OAT_FLOUR);
-            event.accept(BetterStoneAgeItems.COARSE_RICE_FLOUR);
-            event.accept(BetterStoneAgeItems.COARSE_RYE_FLOUR);
-            event.accept(BetterStoneAgeItems.COARSE_WHEAT_FLOUR);
-
-            event.accept(BetterStoneAgeItems.PORRIDGE);
-
-        }
-        if (event.getTabKey() == TFCCreativeTabs.ROCKS.tab().getKey()) {
-            for (RockCategory rock_category : RockCategory.values()) {
-                event.accept(BetterStoneAgeItems.MULTITOOL_HEADS.get(rock_category));
-            }
-            event.accept(BetterStoneAgeItems.FLINT_AXE_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_HAMMER_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_HOE_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_JAVELIN_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_KNIFE_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_SHOVEL_HEAD);
-            event.accept(BetterStoneAgeItems.FLINT_MULTITOOL_HEAD);
-            
-            event.accept(BetterStoneAgeItems.FLINT_AXE);
-            event.accept(BetterStoneAgeItems.FLINT_HAMMER);
-            event.accept(BetterStoneAgeItems.FLINT_HOE);
-            event.accept(BetterStoneAgeItems.FLINT_JAVELIN);
-            event.accept(BetterStoneAgeItems.FLINT_KNIFE);
-            event.accept(BetterStoneAgeItems.FLINT_SHOVEL);
-            
-            event.accept(BetterStoneAgeItems.STONE_ARROWHEAD);
-            event.accept(BetterStoneAgeItems.FLINT_ARROWHEAD);
-            
-        }
-        
+        BetterStoneAgeCreativeTabs.addCreative(event);
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
